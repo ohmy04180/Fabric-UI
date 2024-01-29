ee3accd26e263110fd3fdd15b40cfcba0179dda0
ohmy04180 <ohmy0418@mobigen.com>
2024-01-29 10:05:12 +0900

    [FAB2023-461] feat: color theme 정의

--- fabric-ui/src/components/DesignGuide.vue
+++ fabric-ui/src/components/DesignGuide.vue
@@ -2,6 +2,14 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>디자인 가이드를 보여줄 페이지 입니다.</p>
+    <ol class="test">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ol>
   </div>
 </template>
 
