8e5f44b1af449a5c6bf17554427d27bc057f0916
ohmy04180 <ohmy0418@mobigen.com>
2024-01-29 10:39:46 +0900

    [FAB2023-459] feat: tailwindcss 커스터마이징 참고 문서 주석 추가

--- fabric-ui/tailwind.config.js
+++ fabric-ui/tailwind.config.js
@@ -1,4 +1,8 @@
 // tailwind.config.js
+// https://tailwindcss.com/docs/adding-custom-styles#removing-unused-custom-css
+// https://tailwindcss.com/docs/theme
+// https://tailwindcss.com/docs/configuration
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
