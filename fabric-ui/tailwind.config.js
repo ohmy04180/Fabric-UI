681433543d31aed04dba811db39321bbd4a33cac
ohmy04180 <ohmy0418@mobigen.com>
2024-02-01 15:58:45 +0900

    [FAB2023-459] feat: 코드 정리

--- fabric-ui/tailwind.config.js
+++ fabric-ui/tailwind.config.js
@@ -1,7 +1,4 @@
 // tailwind.config.js
-// https://tailwindcss.com/docs/adding-custom-styles#removing-unused-custom-css
-// https://tailwindcss.com/docs/theme
-// https://tailwindcss.com/docs/configuration
 
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
