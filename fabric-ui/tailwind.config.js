4a2439de0ce7336d5455e846f014e2ccc7e6763b
jungseolhwa <dhqkfskrwl@naver.com>
2024-02-07 15:47:02 +0900

    [FAB2023-464]pretendard 폰트 정의 추가 및 기존 예시 폰트 삭제

--- fabric-ui/tailwind.config.js
+++ fabric-ui/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sans': ['Pretendard','Segoe UI', 'Open Sans', 'Helvetica Neue'],
+        'mono': ['Consolas', 'Menlo', 'Courier', 'monospace']
+      },
+    }
   },
   variants: {
     extend: {}
