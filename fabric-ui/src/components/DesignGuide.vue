5be6bfd778069dd1869f1890bf7a4cec6580d42e
jungseolhwa <dhqkfskrwl@naver.com>
2024-02-07 17:26:59 +0900

    [FAB2023-464] 디자인 가이드 중간저장2

--- fabric-ui/src/components/DesignGuide.vue
+++ fabric-ui/src/components/DesignGuide.vue
@@ -16,10 +16,7 @@
     <section class="guide-section">
       <h2 class="section-title">shadows</h2>
       <ol class="test shadows">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="(shadow, index) in shadows" :key="index">{{ shadow }}</li>
       </ol>
     </section>
     <section class="guide-section">
@@ -28,16 +25,26 @@
         <p>데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
         <p>데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
       </div>
-      <p></p>
     </section>
   </div>
 </template>
 
 <script>
 export default {
+
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      shadows: [
+        'shadow-sm',
+        'shadow-md',
+        'shadow-lg',
+        'shadow-xl'
+      ]
+    };
   }
 };
 </script>
